8c6763dd5cebfcee30dccee6726a183ae804e1dc
uetiari <arianeueti@gmail.com>
2021-04-06 17:46:25 -0300

    Consumo de API e início de resposividade

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,56 @@
     <Logo />
     <!-- <Logo dark-background /> -->
     <h1 class="leads__title">Leads</h1>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Company Name</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+async function getContent() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+
+    show(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getContent()
+
+function show(users) {
+  let output = ''
+  for (let user of users) {
+    output += `
+    <tr>
+      <td>${user.name}</td>
+      <td>${user.email}</td>
+      <td>${user.phone}</td>
+      <td>${user.company.name}</td>
+      <td>More info</td>
+      <hr />
+    </tr> <hr />
+    `
+  }
+
+  document.querySelector('tbody').innerHTML = output
+}
 </script>
 
 <style lang="scss" scoped="true">
